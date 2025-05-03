a71249e7cf4fe0c0e20719a42d7c0d91dd9bb776
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-03 01:30:13 -0700

    Revert "update chart"
    This reverts commit 0fc1072567089d97ee117843c2ffc0cf34b28f8b.

--- scripts/charts.js
+++ scripts/charts.js
@@ -9,40 +9,16 @@ export default class ChartBuilder {
 
     // Method to create the chart
     build() {
-        // const ctx = document.getElementById(this.canvasId).getContext('2d');
-        
+        const ctx = document.getElementById(this.canvasId).getContext('2d');
 
-        // // Create the chart using Chart.js
-        // this.chart = new Chart(ctx, {
-        //     type: this.chartType, // Type of the chart (line, bar, etc.)
-        //     data: this.data, // Data to be displayed
-        //     options: this.options // Configuration options for the chart
-        // });
+        // Create the chart using Chart.js
+        this.chart = new Chart(ctx, {
+            type: this.chartType, // Type of the chart (line, bar, etc.)
+            data: this.data, // Data to be displayed
+            options: this.options // Configuration options for the chart
+        });
 
-        // return this.chart;
-
-        const canvas = document.getElementById(this.canvasId);
-    if (!canvas) {
-        console.error(`Canvas with ID "${this.canvasId}" not found.`);
-        return;
-    }
-
-    const ctx = canvas.getContext('2d');
-
-    // Destroy existing chart on this canvas if one exists
-    if (ChartBuilder.chartInstances[this.canvasId]) {
-        ChartBuilder.chartInstances[this.canvasId].destroy();
-    }
-
-    const newChart = new Chart(ctx, {
-        type: this.chartType,
-        data: this.data,
-        options: this.options
-    });
-
-    ChartBuilder.chartInstances[this.canvasId] = newChart;
-
-    return newChart;
+        return this.chart;
     }
 
     // Method to update the chart data
