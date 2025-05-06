1fa29fd1434d706d10906e2e21881f9a46264b20
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-05 23:33:28 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -224,6 +224,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Fetch data for average event travel distance
         app.fetchData(url).then(data => {
+            console.log("Fetched Data:", data);
+
+            const yData = data.map(event => event.avg_distance_miles);
+            const xData = data.map(event => event.EVENT_NAME);
+        
+            console.log("xData:", xData);
+            console.log("yData:", yData);
+
             // Create a variable to store information about graph
             const { chartData, options } = app.setChartOptions('Average Distance Traveled Per Event', 'Events', 'Average Distance Traveled in Miles', data.map(event => event.EVENT_NAME), data.map(event => event.avg_distance_miles));
 
