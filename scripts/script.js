cdd67cb155eded8d43a9f26103b2555a14bd20cd
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-05 23:40:33 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 import ChartBuilder from "./charts.js";
 
-// Register annotation plugin for Chart.js
-Chart.register(window['chart.js-plugin-annotation']);
-
 // Global element variables
 const tierDropRef = document.querySelector("#tier");
 const countryDropRef = document.querySelector("#country");
