1dcb98d317973e79b17227b030f6c099f2887835
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-08 03:07:28 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -196,13 +196,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 isCount: event.MEMBERS_IN_STATE
             }));
 
-            filtersAdded = () => {
-                if(tierDropRef.value != null){ 
-                    return "by tier";
-                }
-            }
+            // filtersAdded = () => {
+            //     if(tierDropRef.value != null){ 
+            //         return "by tier";
+            //     }
+            // }
             
-
             // Variable to store the combined data
             const { chartData, options } = app.setChartOptions('Average Distance Traveled Per Event', 'Events', 'Average Distance Traveled in Miles', xLabels, yData);
 
