65bc0c0c1ad38f3bcf237da4f239a3492b39e03a
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-07 22:48:37 -0700

    Update fast.js

--- scripts/fast.js
+++ scripts/fast.js
@@ -8,29 +8,23 @@ const applyFiltersButton = document.querySelector("#apply-filters");
 const chartUtils = new ChartUtilities();
 /* -------- GLOBAL VARIABLES END -------- */
 
-/* -------- CLASS DEFINITIONS -------- */
-class ChartUtilities {
-    constructor() { }
-
-    // Method to set chart options based on the data
-    async fetchData(url) {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error("Network response was not ok " + response.statusText);
-            }
-            return await response.json();
-        } catch (error) {
-            return console.error("Fetch error: ", error);
-        }
-    }
-}
-
-
 /* -------- INITIALIZATION -------- */
 document.addEventListener("DOMContentLoaded", async () => {
     updateFilters();
 });
+
+// Method to set chart options based on the data
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+        return await response.json();
+    } catch (error) {
+        return console.error("Fetch error: ", error);
+    }
+}
 
 async function updateFilters() {
     const filters = await chartUtils.fetchData('./PHP/handlers/getFilters.php');
