102d57a9a17ba8f6dd7f211bf41c11f5ed61f4dc
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-09 21:39:28 -0700

    Update fast.js

--- scripts/fast.js
+++ scripts/fast.js
@@ -152,11 +152,11 @@ async function buildEventChart() {
     let title = "Average Distance Traveled Per Event";
 
     // Edit the title based on selected filters
-    title += selectedTier ? `\n Tier: ${selectedTier}` : "";
-    title += selectedCountry ? `\n Country: ${selectedCountry}` : "";
-    title += selectedState ? `\n State: ${selectedState}` : "";
-    title += startDate ? `\n Start Date: ${startDate}` : "";
-    title += endDate ? `\n End Date: ${endDate}` : "";
+    title += selectedTier ? `<br> Tier: ${selectedTier}` : "";
+    title += selectedCountry ? `<br> Country: ${selectedCountry}` : "";
+    title += selectedState ? `<br> State: ${selectedState}` : "";
+    title += startDate ? `<br> Start Date: ${startDate}` : "";
+    title += endDate ? `<br> End Date: ${endDate}` : "";
 
     // Create the chart instance
     eventChart = new Chart(ctx, {
