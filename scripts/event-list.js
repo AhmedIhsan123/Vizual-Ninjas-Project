b41918b738b515d798fd93e87c22d7901181d957
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-15 22:47:06 -0700

    Script updates

--- scripts/event-list.js
+++ scripts/event-list.js
@@ -1,16 +1,16 @@
 window.fetchData("./PHP/events.php").then(data => {
-    const table = document.getElementById("event-table");
+    const tableBody = document.querySelector("#event-table tbody");
     table.innerHTML = ""; // Clear existing rows
+
+    console.log(data);
 
     data.forEach(event => {
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${event.EVENT_ID}</td>
-            <td>${event.EVENT_NAME}</td>
-            <td>${event.DATE_EVENT_END}</td>
-            <td>${event.EVENT_COUNTRY_ID}</td>
+            <td>${event.EVENT_NAME}</td>>
             <td>${event.EVENT_TIER_ID}</td>
         `;
-        table.appendChild(row);
+        tableBody.appendChild(row);
     });
 });
