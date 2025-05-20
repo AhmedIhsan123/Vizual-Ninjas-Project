f9e309b43ca9aeb7b04d04d88d0900f2dd49527d
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-19 22:15:00 -0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -3,14 +3,13 @@ import { fetchData } from "./utils.js";
 // Set the initial view to a specific location and zoom level
 const map = L.map('mapid2').setView([39.5, -98.35], 4);
 
+// Add a tile layer to the map (OpenStreetMap tiles)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors',
+}).addTo(map);
+
 
 export async function initMap() {
-    // Create a map instance
-    // Add a tile layer to the map (OpenStreetMap tiles)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors',
-    }).addTo(map);
-
     // Fetch data from the server
     fetchData("./PHP/events.php").then(data => {
         // Loop through the data and add markers to the map
