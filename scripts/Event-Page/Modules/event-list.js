9df68c42b9210034d55ce5aef40ea61fd6488001
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-22 12:48:58 -0700

    added ids/scripts for event page

---
+++ scripts/Event-Page/Modules/event-list.js
@@ -0,0 +1,23 @@
+// Import statements
+import { fetchData } from "../../utils.js";
+
+// A export function to initialize the list table
+export async function initList() {
+    // Fetch data from the server and populate the table
+    await fetchData("./PHP/events.php").then(data => {
+        const tableBody = document.querySelector(".event-list-container tbody");
+        tableBody.innerHTML = ""; // Clear existing rows
+
+        // Populate the table with data
+        data.forEach(event => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${event.EVENT_ID}</td>
+            <td>${event.EVENT_NAME}</td>
+            <td>${event.EVENT_TIER_ID}</td>
+            <td>${event.DATE_EVENT_END}</td>
+        `;
+            tableBody.appendChild(row);
+        });
+    });
+}
