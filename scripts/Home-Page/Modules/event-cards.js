fc9b77dc69e75efe77cdfe069ac3a7334654ee6d
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-23 03:19:37 -0700

    Update event-cards.js

--- scripts/Home-Page/Modules/event-cards.js
+++ scripts/Home-Page/Modules/event-cards.js
@@ -1,8 +1,31 @@
 import { eventList } from "../home-script.js";
 
 export function populateCards() {
+    // Get the top 10 most popular events and store them in a list
     const sortdEvents = eventList.sort((a, b) => b.TOTAL_MEMBERS - a.TOTAL_MEMBERS).slice(0, 10);
 
-    console.log(sortdEvents);
+
+    // For each event, create a card
+    sortdEvents.forEach(event => {
+        createCard(event.EVENT_NAME, event.EVENT_TIER_ID, event.TOTAL_MEMBERS, event.AVG_TRAVEL_DISTANCE_MILES);
+    });
+}
+
+// Helper function to create a card
+function createCard(eventTitle, eventTierID, eventTotal, eventTravelDistance) {
+    const parentRef = document.getElementById("event-cards-container");
+    const cardRef = document.createElement("div");
+    const cardTitleRef = document.createElement("p");
+    const cardTierRef = document.createElement("p");
+    const cardTotalRef = document.createElement("p");
+    const cardDistanceRef = document.createElement("p");
+
+    cardTitleRef.textContent = eventTitle;
+    cardTierRef.textContent = `Tier ${eventTierID}`;
+    cardTotalRef.textContent = `Players Attending: ${eventTotal}`;
+    cardDistanceRef.textContent = `Average Travel Distance: ${eventTravelDistance}`;
+
+    cardRef.appendChild(cardTitleRef, cardTierRef, cardTotalRef, cardDistanceRef);
+    parentRef.appendChild(cardRef);
 
 }
