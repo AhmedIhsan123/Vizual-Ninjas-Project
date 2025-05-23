139959b26da24c51ffdcebd740642907d26bd8fd
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-23 04:54:30 -0700

    Update event-map.js

--- scripts/Event-Page/Modules/event-map.js
+++ scripts/Event-Page/Modules/event-map.js
@@ -1,8 +1,3 @@
-/* --------------- IMPORTS --------------- */
-
-
-
-
 // Map Initialization
 const map = L.map('mapid').setView([39.5, -98.35], 4);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -34,44 +29,3 @@ export function updateMap(eventsToDisplay) {
         map.setView([39.5, -98.35], 4);
     }
 }
-
-
-
-
-/* --------------- ORIGINAL CODE --------------- */
-
-/*
-
-// Map Initialization
-const map = L.map('mapid').setView([39.5, -98.35], 4);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
-}).addTo(map);
-
-function updateMap(eventsToDisplay) {
-    map.eachLayer(layer => {
-        if (layer instanceof L.Marker) map.removeLayer(layer);
-    });
-
-    eventsToDisplay.forEach(event => {
-        L.marker([event.EVENT_LATITUDE, event.EVENT_LONGITUDE])
-            .addTo(map)
-            .bindPopup(`${event.EVENT_NAME} in ${event.EVENT_CITY}, ${event.EVENT_STATE_ID}, ${event.COUNTRY_ID}`);
-    });
-
-    // If only one event is filtered, zoom to it
-    if (eventsToDisplay.length === 1) {
-        map.flyTo([eventsToDisplay[0].EVENT_LATITUDE, eventsToDisplay[0].EVENT_LONGITUDE], 8);
-    } else if (eventsToDisplay.length > 1) {
-        // If multiple events, fit bounds around them
-        const group = new L.featureGroup(eventsToDisplay.map(event =>
-            L.marker([event.EVENT_LATITUDE, event.EVENT_LONGITUDE])
-        ));
-        map.flyToBounds(group.getBounds(), { padding: L.point(50, 50) });
-    } else {
-        // If no events, reset view to initial
-        map.setView([39.5, -98.35], 4);
-    }
-}
-
-*/
