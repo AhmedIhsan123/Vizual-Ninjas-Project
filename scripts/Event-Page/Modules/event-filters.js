a046d6d7a69ec4d2959ac0293109cfee43309572
sodatax <62825932+sodatax@users.noreply.github.com>
2025-05-23 04:50:34 -0700

    Update event-filters.js

--- scripts/Event-Page/Modules/event-filters.js
+++ scripts/Event-Page/Modules/event-filters.js
@@ -1,5 +1,6 @@
 /* --------------- IMPORTS --------------- */
 import { fetchData } from "../../utils.js";
+import { updateMap } from "./event-map.js";
 
 /* --------------- GLOBAL VARIABLES --------------- */
 const resetFiltersButton = document.querySelector("#reset-filters");
@@ -130,37 +131,8 @@ export async function initEventStats() {
         }
     });
 
-    // Map Initialization
-    const map = L.map('mapid').setView([39.5, -98.35], 4);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors',
-    }).addTo(map);
-
-    function updateMap(eventsToDisplay) {
-        map.eachLayer(layer => {
-            if (layer instanceof L.Marker) map.removeLayer(layer);
-        });
-
-        eventsToDisplay.forEach(event => {
-            L.marker([event.EVENT_LATITUDE, event.EVENT_LONGITUDE])
-                .addTo(map)
-                .bindPopup(`${event.EVENT_NAME} in ${event.EVENT_CITY}, ${event.EVENT_STATE_ID}, ${event.COUNTRY_ID}`);
-        });
-
-        // If only one event is filtered, zoom to it
-        if (eventsToDisplay.length === 1) {
-            map.flyTo([eventsToDisplay[0].EVENT_LATITUDE, eventsToDisplay[0].EVENT_LONGITUDE], 8);
-        } else if (eventsToDisplay.length > 1) {
-            // If multiple events, fit bounds around them
-            const group = new L.featureGroup(eventsToDisplay.map(event =>
-                L.marker([event.EVENT_LATITUDE, event.EVENT_LONGITUDE])
-            ));
-            map.flyToBounds(group.getBounds(), { padding: L.point(50, 50) });
-        } else {
-            // If no events, reset view to initial
-            map.setView([39.5, -98.35], 4);
-        }
-    }
+    // Update the map with the filtered events
+    updateMap(filteredEvents);
 }
 
 
