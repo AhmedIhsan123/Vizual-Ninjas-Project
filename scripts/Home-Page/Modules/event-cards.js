ecdcf1b3b47e491958a5a0492517307bfa4e0fe4
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-23 03:00:13 -0700

    Update event-cards.js

--- scripts/Home-Page/Modules/event-cards.js
+++ scripts/Home-Page/Modules/event-cards.js
@@ -1,10 +1,8 @@
 import { eventList } from "../home-script.js";
 
 export function populateCards() {
+    const sortdEvents = eventList.sort((a, b) => a.TOTAL_MEMBERS - b.TOTAL_MEMBERS);
 
-
-    console.log(eventList);
-
-
+    console.log(sortdEvents);
 
 }
