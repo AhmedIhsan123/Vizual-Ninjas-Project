5ccaad5e2b59839b3e8ab3760ea50203df818157
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-23 23:22:25 -0700

    Changes

--- scripts/Event-Page/Modules/event-stats.js
+++ scripts/Event-Page/Modules/event-stats.js
@@ -1,13 +1,8 @@
 // event-stats.js
-
 const totalPlayersRef = document.querySelector("#total-players");
 const averageDistanceRef = document.querySelector("#average-distance");
-const greaterThanRef = document.querySelector("#greater-than");
-const lessThanRef = document.querySelector("#less-than");
 const outOfStateRef = document.querySelector("#out-of-state");
 const inStateRef = document.querySelector("#in-state");
-const maxRef = document.querySelector("#max-dist");
-const minRef = document.querySelector("#min-dist");
 
 export function fillCards(event) {
     totalPlayersRef.innerHTML = `${event.TOTAL_MEMBERS} Players`;
@@ -16,15 +11,4 @@ export function fillCards(event) {
     inStateRef.innerHTML = `${event.MEMBERS_IN_STATE} In State`;
     maxRef.innerHTML = `${getMax(event)} Miles`;
     minRef.innerHTML = `${Math.min(...event.MEMBERS.map(m => m.AVG_TRAVEL_DISTANCE_MILES))} Miles`;
-}
-
-function getMax(event) {
-    let max = 0;
-    event.forEach(element => {
-        if (element.AVG_TRAVEL_DISTANCE_MILES > max) {
-            max = element.AVG_TRAVEL_DISTANCE_MILES;
-        }
-    });
-
-    return max;
 }
