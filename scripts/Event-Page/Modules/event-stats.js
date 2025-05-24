a5d6c6f010340b5749561c38ff50db5c95b67a61
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-24 04:14:09 -0700

    Update event-stats.js

--- scripts/Event-Page/Modules/event-stats.js
+++ scripts/Event-Page/Modules/event-stats.js
@@ -7,8 +7,8 @@ const moreThanRef = document.querySelector("#more-than");
 const lessThanRef = document.querySelector("#less-than");
 const outOfStateRef = document.querySelector("#out-of-state");
 const inStateRef = document.querySelector("#in-state");
-const maxDistanceRef = document.querySelector("max-dist");
-const minDistanceRef = document.querySelector("min-dist");
+const maxDistanceRef = document.querySelector("#max-dist");
+const minDistanceRef = document.querySelector("#min-dist");
 
 let members = [];
 
