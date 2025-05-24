e745bf77d77dae90f6a8f79d3b0a2d1fdf3684b6
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-23 22:23:53 -0700

    Update event-script.js

--- scripts/Event-Page/event-script.js
+++ scripts/Event-Page/event-script.js
@@ -1,4 +1,4 @@
-/* --------------- IMPORTS --------------- /
+/* --------------- IMPORTS --------------- */
 import { initEventStats, updateFilters } from "./Modules/event-filters.js";
 import { updateChartsAndStats } from "./event-charts.js";
 import { fetchData } from "../../utils.js";
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Build the event stats
     await initEventStats();
 
-    // // Update the filters
-    // await updateFilters();
+    // Update the filters
+    await updateFilters();
 
-    // // Initial call to update charts and stats (you might want to refine this based on when you want this data)
-    // const events = await fetchData("./PHP/events.php"); // Fetch all events initially
-    // if (events && events.length > 0) {
-    //     await updateChartsAndStats(events);
-    // }
+    // Initial call to update charts and stats (you might want to refine this based on when you want this data)
+    const events = await fetchData("./PHP/events.php"); // Fetch all events initially
+    if (events && events.length > 0) {
+        await updateChartsAndStats(events);
+    }
     // Initial call to update charts and stats (you might want to refine this based on when you want this data)
     if (events && events.length > 0) {
         await updateChartsAndStats(events);
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 
-/ --------------- ORIGINAL CODE --------------- */
+/* --------------- ORIGINAL CODE --------------- */
 // // Initial Data Fetch and Render on Page Load
 // document.addEventListener('DOMContentLoaded', async () => {
 //     try {
