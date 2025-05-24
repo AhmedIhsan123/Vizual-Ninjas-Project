bbd3c3ce9fd0e489830a4fc5d5d41af3896b5718
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-24 03:50:45 -0700

    Work on stats

--- scripts/Event-Page/Modules/event-stats.js
+++ scripts/Event-Page/Modules/event-stats.js
@@ -1,14 +1,29 @@
+import { fetchData } from "../../utils.js";
+
 // event-stats.js
 const totalPlayersRef = document.querySelector("#total-players");
 const averageDistanceRef = document.querySelector("#average-distance");
+const moreThanRef = document.querySelector("#more-than");
+const lessThanRef = document.querySelector("#less-than");
 const outOfStateRef = document.querySelector("#out-of-state");
 const inStateRef = document.querySelector("#in-state");
+const maxDistanceRef = document.querySelector("max-dist");
+const minDistanceRef = document.querySelector("min-dist");
 
-export function fillCards(event) {
+let members = [];
+
+export async function fillCards(event) {
+    const returnedList = await fetchData(`./PHP/handlers/getMembers.php?event_id=${event.EVENT_ID}`);
+    members = returnedList;
+
+    console.log(members);
+
     totalPlayersRef.innerHTML = `${event.TOTAL_MEMBERS} Players`;
     averageDistanceRef.innerHTML = `${event.AVG_TRAVEL_DISTANCE_MILES} Miles`;
     outOfStateRef.innerHTML = `${event.MEMBERS_OUT_OF_STATE} Players`;
     inStateRef.innerHTML = `${event.MEMBERS_IN_STATE} Players`;
-    maxRef.innerHTML = `${getMax(event)} Miles`;
-    minRef.innerHTML = `${Math.min(...event.MEMBERS.map(m => m.AVG_TRAVEL_DISTANCE_MILES))} Miles`;
+
+
+
+
 }
