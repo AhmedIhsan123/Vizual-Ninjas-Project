2281aa8ed086f9676fc2336912c6589174af7f1c
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-23 23:25:58 -0700

    Touches

--- scripts/Event-Page/Modules/event-stats.js
+++ scripts/Event-Page/Modules/event-stats.js
@@ -7,8 +7,8 @@ const inStateRef = document.querySelector("#in-state");
 export function fillCards(event) {
     totalPlayersRef.innerHTML = `${event.TOTAL_MEMBERS} Players`;
     averageDistanceRef.innerHTML = `${event.AVG_TRAVEL_DISTANCE_MILES} Miles`;
-    outOfStateRef.innerHTML = `${event.MEMBERS_OUT_OF_STATE} Out of State`;
-    inStateRef.innerHTML = `${event.MEMBERS_IN_STATE} In State`;
+    outOfStateRef.innerHTML = `${event.MEMBERS_OUT_OF_STATE} Players`;
+    inStateRef.innerHTML = `${event.MEMBERS_IN_STATE} Players`;
     maxRef.innerHTML = `${getMax(event)} Miles`;
     minRef.innerHTML = `${Math.min(...event.MEMBERS.map(m => m.AVG_TRAVEL_DISTANCE_MILES))} Miles`;
 }
