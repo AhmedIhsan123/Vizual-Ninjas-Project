30c2821b8d0a33b8a2456a9d3daf2479628406f6
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-24 22:24:15 -0700

    Update event-page-map.js

--- scripts/Event-Page/Modules/event-page-map.js
+++ scripts/Event-Page/Modules/event-page-map.js
@@ -1,4 +1,4 @@
-import { eventList } from "../../script";
+import { eventList } from "../../script.jss";
 // Local Variables
 // Set the initial view to a specific location and zoom level
 const map = L.map('mapid').setView([45.5, -98.35], 4);
