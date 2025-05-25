51a8906976d14c2021304c441dcd5a8f508605ef
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-24 21:38:57 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./utils.js";
 
 // Global program variables
-export const eventList = await fetchData("../PHP/handlers/events.php");
+export const eventList = await fetchData("../PHP/events.php");
 export const playersList = await fetchData("../PHP/handlers/getMembers.php");
 
 // Fetch all events and players
