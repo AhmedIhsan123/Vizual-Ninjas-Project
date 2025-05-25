be2345bb85e39869dc61b8e5efff6466d4d94c89
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-25 01:12:27 -0700

    Update map.js

--- scripts/Home-Page/Modules/map.js
+++ scripts/Home-Page/Modules/map.js
@@ -23,7 +23,8 @@ export async function initMap() {
             End Date: <strong>${event.DATE_EVENT_END}</strong><br>
             Average Distance Traveled to Event: <strong>${event.AVG_TRAVEL_DISTANCE_MILES} miles</strong><br>
             <br>
-            <em>It seems <strong>${event.MEMBERS_OUT_OF_STATE}</strong> members came from out of state, while only <strong>${event.MEMBERS_IN_STATE}</strong> were coming from in-state. This suggests that members are <strong>${event.MEMBERS_OUT_OF_STATE > event.MEMBERS_IN_STATE ? "more likely" : "less likely"}</strong> to attend events in this area.</em>`);
+            <em>It seems <strong>${event.MEMBERS_OUT_OF_STATE}</strong> members came from out of state, while only <strong>${event.MEMBERS_IN_STATE}</strong> were coming from in-state. This suggests that members are <strong>${event.MEMBERS_OUT_OF_STATE > event.MEMBERS_IN_STATE ? "more likely" : "less likely"}</strong> to attend events in this area.</em>
+            <a href="https://aabualhawa.greenriverdev.com/SDEV280/Statmando-Project/event-page.html?name=${event.EVENT_NAME}" class="button">View Details</a>`);
         // Store marker by a unique key (e.g., event ID or name)
         markers[event.EVENT_NAME] = marker;
 
