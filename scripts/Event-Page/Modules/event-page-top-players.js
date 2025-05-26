112e68c97e8952ce4647ebc3ab38e42937a90018
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-26 01:19:47 -0700

    chagbes

--- scripts/Event-Page/Modules/event-page-top-players.js
+++ scripts/Event-Page/Modules/event-page-top-players.js
@@ -1,32 +1,10 @@
 import { currentMembers } from "./event-page-map.js";
 
-const uniqueDivisions = ["OPEN", "AM1", "AM2", "AM3"];
-const openReference = document.getElementById("OPEN-top-players");
-const am1Reference = document.getElementById("AM1-top-players");
-const am2Reference = document.getElementById("AM2-top-players");
-const am3Reference = document.getElementById("AM3-top-players");
-
+// Local variables
+const parentDivRef = document.getElementById("#top-players");
 
 export function updateTopPlayers() {
-    getTopPlayersByDivision(currentMembers, "OPEN").forEach(player => {
-        openReference.innerHTML += `<li>${player.PLAYER_NAME} - ${player.EVENT_PLACE}</li>`;
-    });
-    getTopPlayersByDivision(currentMembers, "AM1").forEach(player => {
-        am1Reference.innerHTML += `<li>${player.PLAYER_NAME} - ${player.EVENT_PLACE}</li>`;
-    });
-    getTopPlayersByDivision(currentMembers, "AM2").forEach(player => {
-        am2Reference.innerHTML += `<li>${player.PLAYER_NAME} - ${player.EVENT_PLACE}</li>`;
-    });
-    getTopPlayersByDivision(currentMembers, "AM3").forEach(player => {
-        am3Reference.innerHTML += `<li>${player.PLAYER_NAME} - ${player.EVENT_PLACE}</li>`;
-    });
-    console.log(currentMembers);
-    console.log(getTopPlayersByDivision(currentMembers, "OPEN"));
-}
-
-function getTopPlayersByDivision(players, division) {
-    return players
-        .filter(player => player.DIVISION === division)
-        .sort((a, b) => a.EVENT_PLACE - b.EVENT_PLACE)
-        .slice(0, 3); // Get top 3 players
+    // Get all the distinct divisons
+    const division = [...new Set(currentMembers.map(member => member.DIVISION_ID))];
+    console.log("Divisions: ", division);
 }
