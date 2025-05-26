aac8a4d71bc3ea45db5fac0c260a0927150777a9
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-26 00:05:22 -0700

    Update event-page-script.js

--- scripts/Event-Page/event-page-script.js
+++ scripts/Event-Page/event-page-script.js
@@ -1,6 +1,7 @@
 import { initMap } from "./Modules/event-page-map.js";
 import { eventList } from "../script.js";
-import { hideAllEventsExcept } from "./Modules/event-page-map.js";
+import { fillCards } from "./Modules/event-stats.js";
+import { hideAllEventsExcept, plotMemberPins } from "./Modules/event-page-map.js";
 
 const params = new URLSearchParams(window.location.search);
 const eventID = params.get("id");
@@ -11,4 +12,6 @@ await initMap();
 if (eventID) {
     const event = eventList.find(event => (event.EVENT_ID == eventID));
     hideAllEventsExcept(event);
+    await plotMemberPins(event); // Show the members attending event
+    fillCards(event);
 }
