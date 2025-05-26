8b8d22684408ba75dd34b605c4c1fbfc277e6f63
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-25 23:29:13 -0700

    Update event-page-map.js

--- scripts/Event-Page/Modules/event-page-map.js
+++ scripts/Event-Page/Modules/event-page-map.js
@@ -22,4 +22,57 @@ export function initMap() {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors',
     }).addTo(map);
+
+    // Add event markers
+    addEventMarkers();
+}
+
+// A function that adds all the event pins to the map
+export function addEventMarkers() {
+    eventList.forEach(event => {
+        // Store the events coordinates
+        const coordinate = [event.EVENT_LATITIUDE, event.EVENT_LONGITUDE];
+
+        // Store the marker instance
+        const marker = L.marker(coordinate);
+
+        // Bind the popup to the marker
+        marker.bindPopup(`<strong>${event.EVENT_NAME}</strong>`);
+
+        // Add event listner for popup opened
+        marker.on("popupopen", function () {
+            hideAllEventsExcept(event);
+        });
+
+        // Add event listner for popup closed
+        marker.on("popupclose", function () {
+            showAllEvents(event);
+        });
+
+        // Add marker to list of markers
+        map.addLayer(marker); // Adds it to the map
+        eventMarkers[event.EVENT_ID] = marker;
+    });
+}
+
+export function hideAllEventsExcept(event) {
+    // For every marker in the markers list
+    for (const id in eventMarkers) {
+        // Hide all the event pins except for exception
+        if (id != event.EVENT_ID) {
+            // Remove layer (hide pin)
+            map.removeLayer(eventMarkers[id]);
+        }
+    }
+}
+
+export function showAllEvents(event) {
+    // For every marker in the markers list
+    for (const id in eventMarkers) {
+        // Hide all the event pins except for exception
+        if (id != event.EVENT_ID) {
+            // Remove layer (hide pin)
+            map.addLayer(eventMarkers[id]);
+        }
+    }
 }
