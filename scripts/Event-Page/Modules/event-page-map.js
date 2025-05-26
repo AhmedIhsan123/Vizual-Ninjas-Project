b27ff275e7c013420f8ab27f32f77a7dde15186b
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-25 19:02:22 -0700

    Update event-page-map.js

--- scripts/Event-Page/Modules/event-page-map.js
+++ scripts/Event-Page/Modules/event-page-map.js
@@ -85,6 +85,7 @@ export function hideAllEventPins(e) {
         // Remove all pins expect for the selected event
         if (event != e) {
             // Remove all the pins by their event ID
+            console.log(eventMarkers[event.EVENT_ID]);
             map.removeLayer(eventMarkers[event.EVENT_ID]);
         }
     });
