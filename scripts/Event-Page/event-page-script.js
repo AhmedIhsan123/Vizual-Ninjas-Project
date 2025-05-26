ba69e25b50d2bf2ada153da8c8f13d86f6ef6f88
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-05-26 00:03:40 -0700

    Update event-page-script.js

--- scripts/Event-Page/event-page-script.js
+++ scripts/Event-Page/event-page-script.js
@@ -1,14 +1,14 @@
 import { initMap } from "./Modules/event-page-map.js";
 import { eventList } from "../script.js";
+import { hideAllEventsExcept } from "./Modules/event-page-map.js";
 
 const params = new URLSearchParams(window.location.search);
 const eventID = params.get("id");
 
 // Init the map
+await initMap();
+
 if (eventID) {
     const event = eventList.find(event => (event.EVENT_ID == eventID));
-    await initMap(true, event);
-} else {
-    await initMap(false)
+    hideAllEventsExcept(event);
 }
-
