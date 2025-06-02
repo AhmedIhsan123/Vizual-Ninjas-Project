9693890be51344a17051189dfab3c13f75c948ba
Allen R <allen.resulidze@gmail.com>
2025-06-02 12:32:26 -0700

    change label wording

--- scripts/Home-Page/Modules/map.js
+++ scripts/Home-Page/Modules/map.js
@@ -68,6 +68,6 @@ export function focusOnEvent(eventId) {
 export function updateMapLabel(event) {
     const labelElement = document.getElementById('map-event-label');
     if (labelElement) {
-        labelElement.innerHTML = `You're viewing ${event.EVENT_NAME}`;
+        labelElement.innerHTML = `Selected Event: ${event.EVENT_NAME}`;
     }
 }
