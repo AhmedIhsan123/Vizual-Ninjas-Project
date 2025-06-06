80293946321840defda88b2bbda21620de95e54e
AhmedIhsan123 <ihsan.ahmed@student.greenriver.edu>
2025-06-05 23:11:33 -0700

    Update event-page-tchart.js

--- scripts/Event-Page/Modules/event-page-tchart.js
+++ scripts/Event-Page/Modules/event-page-tchart.js
@@ -77,7 +77,7 @@ export function buildTChart() {
     // Count members per state
     const stateCounts = {};
     currentMembers.forEach(member => {
-        const state = member.MEMBER_STATE_PROV || "Unknown";
+        const state = member.MEMBER_STATE_NAME || "Unknown";
         stateCounts[state] = (stateCounts[state] || 0) + 1;
     });
 
